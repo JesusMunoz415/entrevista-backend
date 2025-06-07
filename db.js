598325483b12f2c1b9e89b7e74f270216fbee38a
JesusMunoz415 <jesusmunozmartinez608@gmail.com>
2025-06-06 20:38:08 -0600

    Actualizo conexión a Render con DATABASE_URL

--- db.js
+++ db.js
@@ -1,13 +1,8 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+// Conexión desde variable DATABASE_URL
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect(err => {
   if (err) {
