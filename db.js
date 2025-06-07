8cf83502ac81bc848ce639cf2412c4d8c174ea1c
JesusMunoz415 <jesusmunozmartinez608@gmail.com>
2025-06-06 20:52:41 -0600

    Actualizo conexión a Render con DATABASE_URL

--- db.js
+++ db.js
@@ -1,14 +1,19 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Conexión desde variable DATABASE_URL
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
-db.connect(err => {
+db.connect((err) => {
   if (err) {
-    console.error('❌ Error al conectar a la base de datos:', err);
+    console.error("❌ Error al conectar a la base de datos:", err);
   } else {
-    console.log('✅ Conectado a la base de datos MySQL');
+    console.log("✅ Conectado a la base de datos MySQL");
   }
 });
 
