773c26da0f5e99d0d434b7c154c0712fedb25911
JesusMunoz415 <jesusmunozmartinez608@gmail.com>
2025-06-22 17:04:44 -0600

    Renombro eliminarEntrevista.js → eliminarEntrevistaRoutes.js (fix en Render)

--- routes/eliminarEntrevista.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { eliminarEntrevista } = require('../controllers/eliminarEntrevistaController');
-
-router.post('/', eliminarEntrevista);
-
-module.exports = router;
--- routes/eliminarEntrevistaRoutes.js
+++ routes/eliminarEntrevistaRoutes.js
@@ -1,7 +1,9 @@
+// File: backend/routes/eliminarEntrevista.js
+// This file is part of backend/routes/eliminarEntrevista.js
 const express = require('express');
 const router = express.Router();
-const eliminarEntrevista = require('../controllers/eliminarEntrevistaController');
+const { eliminarEntrevista } = require('../controllers/eliminarEntrevistaController');
 
-router.post('/eliminar-entrevista', eliminarEntrevista);
+router.post('/', eliminarEntrevista);
 
 module.exports = router;
