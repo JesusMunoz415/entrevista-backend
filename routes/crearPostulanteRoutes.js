4ec7b8625d57822ce13df5e3610feb88611a898f
JesusMunoz415 <jesusmunozmartinez608@gmail.com>
2025-06-23 23:20:10 -0600

    Fix ruta crearPostulante y corrige endpoint POST /api/postulantes

--- routes/crearPostulanteRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/crearPostulanteRoute.js
-const express = require('express');
-const router = express.Router();
-const crearPostulante = require('../controllers/crearPostulanteController');
-
-router.post('/crear_postulante', crearPostulante);
-
-module.exports = router;
