9528022d577c0082ef35c7859a6e5c0ddb12aceb
JesusMunoz415 <jesusmunozmartinez608@gmail.com>
2025-07-04 17:59:00 -0600

    Agrego ruta historialEntrevistasRoutes desde cero

--- routes/historialEntrevistasRoutes.js
+++ routes/historialEntrevistasRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { obtenerHistorial } = require('../controllers/historialEntrevistasController');
 
-router.post('/', obtenerHistorial);
+// Ruta para obtener historial de entrevistas
+router.post('/', (req, res, next) => {
+  console.log('📥 [POST] /api/historial');
+  obtenerHistorial(req, res, next);
+});
 
 module.exports = router;
